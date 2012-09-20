faddb2dda52c080f974784a131301a7f1004f2b6
Siwei Yang <YangSiwei880813@Gmail.Com>
2012-09-20 18:51:32 -0400

    nullable working

--- CFG.hs
+++ CFG.hs
@@ -3,7 +3,8 @@ import System.Environment(getArgs)
 import System.IO(openFile, IOMode(ReadMode), Handle, hIsEOF, hGetLine, hPutStrLn)
 
 import Text.Regex(mkRegex, splitRegex)
-import Parser(Rule(Rule))
+import Parser(Rule(Rule), symbolTable, getNullableList, getFirstList)
+import Parser(Symbol(Terminal), productions, joinSymbolWithSymbols)
 
 type FilePath = String
 
@@ -29,4 +30,20 @@ main = do
   putStrLn filename
   lines <- readLines file
   let tokens = [split " " line | line <- lines]
-  print [Rule (head token) (drop 2 token) | token <- tokens, length token > 2]
+  let grammar = [Rule (head token) (drop 2 token) | token <- tokens, length token > 1]
+  let table = symbolTable grammar
+  let notNullableV0 = [Terminal grammar symbolName | Terminal grammar symbolName <- table]
+  let notNullableV1Pair = [(minimum (map length (joinSymbolWithSymbols symbol notNullableV0)), symbol) | symbol <- table]
+  let notNullableV1 = [symbol | (weight, symbol) <- notNullableV1Pair, weight > 0]
+  let notNullableV2Pair = [(minimum (map length (joinSymbolWithSymbols symbol notNullableV1)), symbol) | symbol <- table]
+  let notNullableV2 = [symbol | (weight, symbol) <- notNullableV2Pair, weight > 0]
+  print table
+  print [(symbol, productions symbol) | symbol <- table]
+  print notNullableV0
+  print notNullableV1Pair
+  print notNullableV1
+  print notNullableV2Pair
+  print notNullableV2
+  
+  print (getNullableList table)
+  print (getFirstList grammar)
--- Parser.hs
+++ Parser.hs
@@ -1,11 +1,17 @@
 module Parser where
 
+import Text.Printf(printf)
+import Data.List(sortBy)
 
 data Rule = Rule {root::String, production::[String]} deriving (Show, Eq)
-data Symbol = Terminal Grammar String | NonTernimal Grammar String deriving (Show, Eq)
+data Symbol = Terminal Grammar String | NonTerminal Grammar String deriving (Eq)
+instance Show Symbol where
+  show (Terminal _ symbolName) = printf "Terminal[%s]" symbolName
+  show (NonTerminal _ symbolName) = printf "NonTerminal[%s]" symbolName
 
 type Grammar = [Rule]
 type SymbolTable = [Symbol]
+type FirstTable = [(Symbol, [Symbol])]
 {-
   -- Circular dependency of types
   data Terminal = Terminal String
@@ -15,29 +21,40 @@ type SymbolTable = [Symbol]
 
 name :: Symbol -> String
 name (Terminal _ symbolName) = symbolName
-name (NonTernimal _ symbolName) = symbolName
+name (NonTerminal _ symbolName) = symbolName
 
 symbolTable :: Grammar -> SymbolTable
 symbolTable grammar = terminalList ++ nonTerminalList
   where
     fullSymbolNameList = foldl (\symbolNameList symbolName -> if length [s | s <- symbolNameList, s == symbolName] > 0 then symbolNameList else symbolName:symbolNameList) [] (concat [(root rule):production rule | rule <- grammar])
-    nonTerminalList = [NonTernimal grammar (root rule) | rule <- grammar]    
-    terminalList = [Terminal grammar symbolName | symbolName <- fullSymbolNameList, length [symbol | symbol <- nonTerminalList, name symbol == symbolName] == 0]
+    nonTerminalList = [NonTerminal grammar symbolName | symbolName <- fullSymbolNameList, length [rule | rule <- grammar, root rule == symbolName] /= 0]
+    terminalList = [Terminal grammar symbolName | symbolName <- fullSymbolNameList, length [rule | rule <- grammar, root rule == symbolName] == 0]
 
 productions :: Symbol -> [[Symbol]]
-productions (Terminal _ _) = []
-productions (NonTernimal grammar symbolName) = [[head [symbol | symbol <- table, name symbol == symbolName] | symbolName <- production rule] | rule <- relevantRules]
+productions (Terminal grammar symbolName) = [[Terminal grammar symbolName]]
+productions (NonTerminal grammar symbolName) = [[head [symbol | symbol <- table, name symbol == symbolName] | symbolName <- production rule] | rule <- relevantRules]
   where
     table = symbolTable grammar
     relevantRules = [rule | rule <- grammar, root rule == symbolName]
     relevantSymbolNames = [[symbolName | symbolName <- production rule] | rule <- relevantRules]
 
-getNullableList :: SymbolTable -> [Symbol]
-getNullableList table = symbols
-    where
-        symbols = table
+joinSymbolWithSymbols :: Symbol -> [Symbol] -> [[Symbol]]
+joinSymbolWithSymbols symbol symbols = [[product | product <- production, elem product symbols] | production <- productions symbol]
 
-resolveProduction :: [Symbol] -> [(Symbol, [Symbol])] -> [Symbol] -> Maybe [Symbol]
+buildNotNullableListRecursive :: SymbolTable -> [Symbol] -> [Symbol]
+buildNotNullableListRecursive table notNullable = if length notNullable == length mergedNotNullable then notNullable else buildNotNullableListRecursive table mergedNotNullable
+  where
+    weightedJoinList = [(minimum (map length (joinSymbolWithSymbols symbol notNullable)), symbol) | symbol <- table]
+    newNotNullable = [symbol | (weight, symbol) <- weightedJoinList, weight > 0]
+    mergedNotNullable = foldl (\symbols symbol -> if elem symbol symbols then symbols else symbol:symbols) notNullable newNotNullable
+
+getNullableList :: SymbolTable -> [Symbol]
+getNullableList table = [symbol | symbol <- table, not (elem symbol notNullable)]
+  where
+    notNullable = buildNotNullableListRecursive table [Terminal grammar symbolName | Terminal grammar symbolName <- table]
+        
+
+resolveProduction :: [Symbol] -> FirstTable -> [Symbol] -> Maybe [Symbol]
 resolveProduction nullable firstList [] = Just []
 resolveProduction nullable firstList (first:rest) = case (length theNullableList > 0, nextFirstMaybe, length theFirstList > 0) of
                                                     (False, _, True) -> Just theFirst
@@ -50,29 +67,39 @@ resolveProduction nullable firstList (first:rest) = case (length theNullableList
     nextFirstMaybe = resolveProduction nullable firstList rest
     theFirstList = [theFirst | (symbol, theFirst) <- firstList, symbol == first]
     theFirst = head theFirstList
-    
 
-
-buildFirstList :: [Symbol] -> [(Symbol, [Symbol])] -> Symbol -> [(Symbol, [Symbol])]
+buildFirstList :: [Symbol] -> FirstTable -> Symbol -> FirstTable
 buildFirstList nullable firstList (Terminal grammar symbolName) = (symbol, [symbol]) : firstList
   where
     symbol = Terminal grammar symbolName
-buildFirstList nullable firstList (NonTernimal grammar symbolName) =
+buildFirstList nullable firstList (NonTerminal grammar symbolName) =
   if foldl (\isResolvable firstFromProduction -> if isResolvable then firstFromProduction /= Nothing else False) True firstFromProductionList
   then (symbol, mergedFirstFromProductionList):firstList
   else firstList
   where
-    symbol = NonTernimal grammar symbolName
+    symbol = NonTerminal grammar symbolName
     productionList = productions symbol
     firstFromProductionList = map (resolveProduction nullable firstList) productionList
     mergedFirstFromProductionList = foldl (\mergedFirst (Just firstFromProduction) -> mergedFirst ++ firstFromProduction) [] firstFromProductionList
 
+buildFirstListRecursive :: [Symbol] -> FirstTable -> [Symbol] -> FirstTable
+buildFirstListRecursive nullable firstList symbols = case length newFirstList > length firstList of
+                                                      True -> buildFirstListRecursive nullable firstList newSymbols
+                                                      False -> firstList
+  where
+    potentialFirstList = [(buildFirstList nullable firstList symbol, symbol) | symbol <- symbols] :: [(FirstTable, Symbol)]
+    newFirstPair = head (sortBy (\(listA, _) (listB, _) -> compare (length listB) (length listA)) potentialFirstList)
+    (newFirstList, newFirstSymbol) = newFirstPair
+    newSymbols = [symbol | symbol <- symbols, symbol /= newFirstSymbol]
+
+getFirstList :: Grammar -> FirstTable
+getFirstList grammar = buildFirstListRecursive nullable [] table
+  where
+    table = symbolTable grammar
+    nullable = getNullableList table
+    --(\x -> f x x) (\x -> f x x)
 {- checking for left recursion is necessary -}
 {-
-getFirstList :: Grammar -> [(Symbol, [Symbol]]
-getFirstList = [Symbol]
-  where
-    rules = grammar
 
 getFollowList :: Grammar -> [Symbol]
 getFollowList [Symbol]
